c99f645779dd35fb51d4365212bde0c9063920f7
nouc23 <nouc23@gmail.com>
2017-04-13 17:40:34 +0200

    clean comments

--- src/client/app/shared/office/office.component.ts
+++ src/client/app/shared/office/office.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { OfficeInterface } from "../config/office.interface";
 
-/**
- * This class represents the navigation bar component.
- */
 @Component({
   moduleId: module.id,
   selector: 'fart-office',
